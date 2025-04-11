94864071f92ddd75aafa16b3ac1c00ca2dacfd16
Fabricio Antonio Godoy <FAGodoy@dmenargas.gov.ar>
2025-04-11 16:18:26 -0300

    sasrasa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import {Footer} from "../components/footer/footer";
+/* import {NavBar} from "../components/navBar/navBar"; */
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+     {/*  <NavBar /> */}
+
       <body className="antialiased">
         <Main />
         <NextScript />
       </body>
+      <Footer />
     </Html>
   );
 }
---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+    return (
+      <div>
+        <h1>Sobre mí</h1>
+        <p>Esta es la página about.</p>
+
+        <a href="/">
+        Ir a INICIO
+      </a>
+
+
+      </div>
+    );
+  }
